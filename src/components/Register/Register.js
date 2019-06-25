c36855752db082f9a7f3a52323d802714a3e574d
Jacob <jacobdeansd@gmail.com>
2019-06-25 14:05:59 -0700

    fix

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://fathomless-spire-76366.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
