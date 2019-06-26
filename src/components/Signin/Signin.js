cb36a3b696b066d3eb974dae878b6b0f4144a701
Jacob <jacobdeansd@gmail.com>
2019-06-26 12:20:21 -0700

    deploy

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://still-hollows-22389.herokuapp.com/signin', {
+    fetch('https://lit-wave-11776.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
