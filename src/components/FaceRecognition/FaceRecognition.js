94fc1ef1064c26601f125c8ed8cfc64361c8b4f6
Jacob <jacobdeansd@gmail.com>
2019-08-07 20:08:00 -0700

    fix bounding box

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -5,8 +5,8 @@ const FaceRecognition = (props) => {
     return(
         <div className='center ma'>
             <div className='absolute mt2'>
-                <img id ='inputimage' src={props.imageUrl} alt='' width='500px' height='500px' />
-                <div className='bounding_box' style={{top:props.box.topRow, left:props.box.leftCol, bottom:props.box.bottomRow,right:props.box.rightCol}}>
+                <img id ='inputimage' src={props.imageUrl} alt='' width='50%' height='50%' />
+                <div className='bounding-box' style={{top:props.box.topRow, left:props.box.leftCol, bottom:props.box.bottomRow,right:props.box.rightCol}}>
                 </div>     
             </div>           
         </div>    
