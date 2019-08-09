3082208a93f4de607c6122368a7b912589073fb6
Jacob <jacobdeansd@gmail.com>
2019-08-08 17:36:08 -0700

    centered

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Rank = ({name, entries}) => {
 	return (
-		<div className="ma4 mt0 center">
+		<div className="ma4 mt0 entry">
 			<div className="white f3">
 				<span className="b f2">{name}, your entry count is </span>
 			</div>
-			<br/>
+			
 			<div className="white f2">
 				{entries}
 			</div>
